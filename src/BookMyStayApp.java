6418ba07881ddd064c72a56e160680fd0a2a0640
Smriti-Prakash <smriti.prakash012@gmail.com>
2026-03-13 15:15:25 +0530

    Use Case 2 - Room Types and Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,8 +1,41 @@
-public class BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("        BOOK MY STAY APPLICATION       ");
-        System.out.println("        Hotel Booking System v1.0      ");
-        System.out.println("Welcome! The application has started successfully.");
+/**
+ * =========================================================
+ * MAIN CLASS – BookMyStayApp
+ * =========================================================
+ *
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * @version 2.1
+ */
 
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====== BOOK MY STAY APP v2.1 ======\n");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 10;
+        int doubleAvailable = 6;
+        int suiteAvailable = 3;
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
